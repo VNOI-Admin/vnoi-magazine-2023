767485922b8dea79132c8cc6254270841eb4ae9e
Tran Quang Loc <quangloctran99@gmail.com>
2023-01-20 10:28:45 +0700

    Add QA name for interviews

--- scripts/preprocess-interview.py
+++ scripts/preprocess-interview.py
@@ -1,4 +1,8 @@
-from sys import stdin
+from sys import stdin, stderr
+import yaml
+import re
+
+stdin.reconfigure(encoding="utf-8-sig")
 
 def escape_latex(text: str) -> str:
     specials = {
@@ -16,5 +20,30 @@ def escape_latex(text: str) -> str:
 
     return "".join(specials.get(s, s) for s in text)
 
-for line in stdin.readlines():
-    print(escape_latex(line))
+lines = list(s.strip() for s in stdin)
+content = '\n'.join(lines)
+match = re.match(r'---\n([\s\S]*?)\n---\n([\s\S]*)', content, re.M)
+
+if match is None:
+    front_matter = '{}'
+    interview_content = content
+else:
+    front_matter = match.group(1)
+    interview_content = match.group(2)
+    
+front_matter_data = yaml.safe_load(front_matter)
+Q = front_matter_data.get('Q', 'Q')
+A = front_matter_data.get('A', 'A')
+
+print(f'\\def\\Qtext{{{Q}}}')
+print(f'\\def\\Atext{{{A}}}')
+
+for line in interview_content.split('\n'):
+    line = line.strip()
+    if line == '':
+        continue
+    line = escape_latex(line)
+    line = re.sub('^Q:', r'\\interviewQ', line)
+    line = re.sub('^A:', r'\\interviewA', line)
+    print(line)
+    print()
